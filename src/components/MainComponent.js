74cf3d3be829c8ed9d3db6364a50c14574c8c43a
Moritz Huber <moritz.huber11@gmx.de>
2020-06-07 08:12:45 +0200

    Week 3 Workshop Assignment including an added AboutComponent and an updated MainComponent.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
@@ -37,6 +38,21 @@ class Main extends Component {
       );
     };
 
+    const campsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -47,7 +63,13 @@ class Main extends Component {
             path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
+          <Route path="/directory/:campsiteId" component={campsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
